114c848656a845d053b86fa1e8a727c466bb7bb3
ARAN-ai-python <67265109+ARAN-ai-python@users.noreply.github.com>
2021-06-26 22:53:10 +0900

    Create 4.Real_Estate_Rent_Divided_Prediction.py
    作成したAIを用いて、テストデータによる予測結果を見やすく表示するプログラム

--- 4.Real_Estate_Rent_Divided_Prediction.py
+++ 4.Real_Estate_Rent_Divided_Prediction.py
@@ -1,10 +1,11 @@
 #必要なライブラリをインポート
+import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 
-#pandasのデータフレームの列表示制限を解除
+#pandasのデータフレームの列表示制限を解除する
 pd.set_option('display.max_rows',None)
 
 
@@ -15,20 +16,20 @@ pd.set_option('display.max_rows',None)
 #RERLで作ったAIを用いて、テストデータの不動産の賃料を予測する関数
 #Real_Estate_Rent_Divided_Prediction
 def RERDP(df):
-    #学習データと教師データの分割
+    #学習データと教師データの分割する
     df_fact = df['賃料']
     df_input = df.drop(['賃料'],axis=1)
     
-    #予測値の生成
+    #予測値の生成する
     df_input = xgb.DMatrix(df_input)
     pred = model.predict(df_input)
     
-    #実値と予測値の表示
+    #実値と予測値の表示する
     df_pred = pd.DataFrame(pred,columns=['予測値'])
     df_final = pd.concat([df_fact,df_pred],axis=1)
     display(df_final)
     
-    #モデルの性能を表示
+    #モデルの性能を表示する
     print('MAE:',mean_absolute_error(df_fact,df_pred))
     print('MSE:',mean_squared_error(df_fact,df_pred))
     print('RMSE:',np.sqrt(mean_squared_error(df_fact,df_pred)))
